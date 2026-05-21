7306d6dda9d036744db59bd0b569c2ef77942799
itsdukey <encryptedsins@gmail.com>
2026-05-21 12:05:22 -0700

    Reduced body width down to 155 from 175 to fix java swing hover tooltip

--- src/main/java/com/ClanActionLogger/ClanActionLoggerConfig.java
+++ src/main/java/com/ClanActionLogger/ClanActionLoggerConfig.java
@@ -20,8 +20,8 @@ public interface ClanActionLoggerConfig extends Config
 
 	@ConfigItem(
 			keyName = "setupNotice",
-			name = "<html><font color='#e0a32d'>Please read the guides below<br>to properly set up the plugin.</font></html>",
-			description = "Scroll to the bottom of the panel to view the setup instructions.",
+			name = "<html><body width='155'><font color='#e0a32d'>Please read the guides below<br>to properly set up the plugin.</font></body></html>",
+			description = "",
 			section = noticeSection,
 			position = 1
 	)
@@ -164,7 +164,7 @@ public interface ClanActionLoggerConfig extends Config
 
 	@ConfigItem(
 			keyName = "guideOptionsText",
-			name = "<html><body width='175'>"
+			name = "<html><body width='155'>"
 					+ "<font color='#A0A0A0' face='sans-serif' size='3'>"
 					+ "These settings verify your authorization and link the plugin to Discord.<br><br>"
 					+ "<b>Clan Name:</b> <i>(Required)</i><br> Locks the plugin to only monitor events for this exact clan.<br><br>"
@@ -172,7 +172,7 @@ public interface ClanActionLoggerConfig extends Config
 					+ "<b>Discord Webhook URL:</b> <i>(Required)</i> The destination channel where your logs will be delivered.<br><br>"
 					+ "<b>Multi-User Support:</b> <i>(Optional)</i> Routes logs through a 3rd party server to prevent duplicate posts when multiple admins are online."
 					+ "</font></body></html>",
-			description = "Explanation of administrative setup fields",
+			description = "",
 			position = 16,
 			section = guideOptionsSection
 	)
@@ -191,7 +191,7 @@ public interface ClanActionLoggerConfig extends Config
 
 	@ConfigItem(
 			keyName = "guideAdminText",
-			name = "<html><body width='175'>"
+			name = "<html><body width='155'>"
 					+ "<font color='#A0A0A0' face='sans-serif' size='3'>"
 					+ "<b><center>Setup Steps:</center></b><br>"
 					+ "<center><b>If you are not Discord Owner/Admin, Contact them for the webhook url and skip to step 3</b></center><br>"
@@ -207,7 +207,7 @@ public interface ClanActionLoggerConfig extends Config
 					+ "6. Turn on multi-user support to prevent duplicate Discord webhook messages, this works by sending logs to a third party server to de-duplicate, prior to sending to discord.<br><br>"
 					+ "This setting runs on a 60 second window, so if you have 2+ clients and/or users running this plugin in your clan and you do any of the configurable actions, it'll protect from double posting for 60 seconds, on matching actions."
 					+ "</font></body></html>",
-			description = "Step-by-step configuration verification checklist",
+			description = "",
 			position = 18,
 			section = guideAdminSection
 	)
@@ -226,14 +226,14 @@ public interface ClanActionLoggerConfig extends Config
 
 	@ConfigItem(
 			keyName = "guideOnlineText",
-			name = "<html><body width='175'>"
+			name = "<html><body width='155'>"
 					+ "<font color='#A0A0A0' face='sans-serif' size='3'>"
 					+ "These options will send discord notifications when you are logged into the Game & Clan on the RSN you entered in \"Admin setup\" .<br><br>"
 					+ "<b>Log Kicks & Bans:</b> Sends Discord notification when an Admin kicks, bans, or expels someone from the Clan Chat channel.<br><br>"
 					+ "<b>Log Invites:</b> Sends Discord notification when a player is successfully invited into the clan.<br><br>"
 					+ "<b>Log Rank Changes:</b> Sends Discord notification when a member is promoted or demoted."
 					+ "</font></body></html>",
-			description = "Explanation of real-time tracking features",
+			description = "",
 			position = 20,
 			section = guideOnlineSection
 	)
@@ -252,14 +252,14 @@ public interface ClanActionLoggerConfig extends Config
 
 	@ConfigItem(
 			keyName = "guideOfflineText",
-			name = "<html><body width='175'>"
+			name = "<html><body width='155'>"
 					+ "<font color='#A0A0A0' face='sans-serif' size='3'>"
 					+ "Compares your Clan List against your last login session to make a comparison of changes since you were last online.<br><br>"
 					+ "<b>RSN(s) for Offline Audit:</b> Specify account(s) name to limit who triggers the audit on login. If left blank, Any account you log into listed in \"Admin Setup\" will trigger an audit.<br><br>"
 					+ "<b>Track Roster Changes:</b> Reports members who joined or left the clan entirely while you were offline. <i>*This does not track who removed them while offline</i><br><br>"
 					+ "<b>Track Rank Changes:</b> Reports members whose ranks were adjusted while you were offline. * <i>This does not track who made changes while offline</>"
 					+ "</font></body></html>",
-			description = "Explanation of offline audit features",
+			description = "",
 			position = 22,
 			section = guideOfflineSection
 	)
